cadd29b93e67bfcf3c9a672f240c7efaa1ad7e68
Youzhu Shi <39037277+daisyworld2018@users.noreply.github.com>
2019-08-03 13:57:01 -0700

    Create BinarySearch.js

---
+++ BinarySearch.js
@@ -0,0 +1,27 @@
+
+//create a function that does binary search
+//assume array has been sorted from small to large
+function binarySearch(arr,num){
+  //create a object whose key is the index number
+  var obj = {};
+  for (var i = 0; i < arr.length; i++){
+    obj[arr[i]] = i;
+  }
+  //interate over the arr
+  var halfArray = arr
+  for(var i = 0; i < arr.length; i++){
+    //compare mid point number to the target number
+    var midNum = halfArray[Math.floor(halfArray.length/2)]
+    
+    //search left if mid point number if bigger than target number, vice versa
+    if(midNum > num){
+      halfArray = halfArray.slice(0,Math.floor(halfArray.length/2));
+    } else if (midNum < num) {
+      halfArray = halfArray.slice(Math.floor(halfArray.length/2)+1,);
+    } else if (midNum === num){
+      return obj[midNum];
+    }
+  }
+};
+
+binarySearch([1, 3, 16, 22, 31, 33, 34],33);
